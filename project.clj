0245fe6705640c5ee48e4669304098419f1266a0
Antonio Terreno <antonio.terreno@gmail.com>
2012-09-18 17:10:15 +0200

    first commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject rest-in-clojure "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [compojure "1.1.3"]]
+  :plugins [[lein-ring "0.7.5"]]
+  :ring {:handler rest-in-clojure.handler/app}
+  :dev-dependencies [[ring-mock "0.1.2"]])
